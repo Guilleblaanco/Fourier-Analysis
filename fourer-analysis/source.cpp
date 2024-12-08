d63ee7d7b3fb7779f5db985bc995dcbf1e264078
Guille Blanco <guilleblanco2015@gmail.com>
2024-12-08 11:09:49 +0100

    added some functionality to create output files without error handlindg yet

--- fourer-analysis/source.cpp
+++ fourer-analysis/source.cpp
@@ -1,3 +1,5 @@
+#include <filesystem>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <complex>
@@ -6,10 +8,17 @@
 #include <iostream>
 
 using namespace std;
+using namespace std::filesystem;
 
+// Reads the file and takes it result to a vector
 void readCSV(const string& filename, vector<double>& time, vector<double>& amplitude) {
     ifstream file(filename);
     string line;
+
+    if (!file.is_open()) {
+        cout << "Input file is empty" << endl;
+        return;
+    }
 
     while (getline(file, line)) {
         stringstream ss(line);
@@ -23,6 +32,7 @@ void readCSV(const string& filename, vector<double>& time, vector<double>& ampli
     }
 }
 
+// Makes the DFT 
 void computeDFT(const vector<double>& signal, vector<complex<double>>& result) {
     if (signal.empty()) {
         cerr << "Error: Signal vector is empty!" << endl;
@@ -43,21 +53,54 @@ void computeDFT(const vector<double>& signal, vector<complex<double>>& result) {
     }
 }
 
+// Takes the DFT as argument and outputs the result in the console
 void processResults(const vector<complex<double>>& dft, double samplingRate) {
     int N = dft.size();
-    for (int k = 0; k < N / 2; ++k) { // Frecuencias positivas
-        double magnitude = abs(dft[k]);
+    for (int k = 0; k < N / 2; ++k) { 
+        double magnitude = abs(dft[k]); // Magnitude: Intensity of the specified frecuency
         double frequency = k * samplingRate / N;
         cout << "Frequency: " << frequency << " Hz, Magnitude: " << magnitude << '\n';
     }
 }
 
-void saveResults(const vector<complex<double>>& dft, double samplingRate, const string& filename) {
-    ofstream file(filename);
+// Generates an output directory
+void ensureOutputDirectory(const string& directory) {
+    if (!exists(directory)) {
+        create_directory(directory);
+        cout << "Created directory" << directory << endl;
+    }
+}
+
+// Generates an unique name for the output file
+string generateOutputFilename(const string& inputFilename, const string& directory) {
+    string baseName = inputFilename.substr(0, inputFilename.find_last_of('.')); // Default name
+    string outputName = directory + "/output_" + baseName + ".csv";
+
+    int counter = 1;
+    while (exists(outputName)) { // If the file exists, add a numerical suffix
+        outputName = directory + "/output_" + baseName + "_" + to_string(counter) + ".csv";
+        ++counter;
+    }
+    return outputName;
+}
+
+// Saves the results in a output.csv file
+void saveResults(const vector<complex<double>>& dft, double samplingRate, const string& inputFilename) {
+    const string outputDir = "outputs";
+    ensureOutputDirectory(outputDir);
+
+    string outputFileName = generateOutputFilename(inputFilename, outputDir);
+    ofstream file(outputFileName);
     int N = dft.size();
-    for (int k = 0; k < N / 2; ++k) {
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file for writing.\n";
+        return;
+    }
+
+    for (int k = 0; k < N / 2; ++k) { // Takes only the meaningful signals (the positive ones)
         double magnitude = abs(dft[k]);
-        double frequency = k * samplingRate / N;
+        double frequency = k * samplingRate / N;    
         file << frequency << "," << magnitude << '\n';
     }
     file.close();
@@ -65,15 +108,16 @@ void saveResults(const vector<complex<double>>& dft, double samplingRate, const 
 
 int main() {
     vector<double> time, amplitude;
-    readCSV("signal.csv", time, amplitude);
+    string inputFileName = "signal_TVG.csv"; // Put the name of the input file here
+    readCSV(inputFileName, time, amplitude);
 
     vector<complex<double>> dftResult;
     computeDFT(amplitude, dftResult);
 
-    double samplingRate = 1000.0; // Hz (ejemplo)
+    double samplingRate = 1000.0; // Arbitrary, you may change it depending on the resolution needed
     processResults(dftResult, samplingRate);
 
-    saveResults(dftResult, samplingRate, "output.csv");
+    saveResults(dftResult, samplingRate, inputFileName);
 
     return 0;
 }
